816ea2c127da0d4903380f93538f78bfded3a320
Michael <michaelraffinpaculba@gmail.com>
2023-03-13 19:59:25 +0800

    Adding Facebook Messenger

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,25 @@ function MyApp({ Component, pageProps }) {
  `}
 </Script>
 
+
+
+
+<Script >
+{`window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v12.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));`}
+  </Script>
   <Component {...pageProps} />;
   </>
 }
